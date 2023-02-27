23b572cb477b6673d431a1a61ba02626b520e5ce
Beatrice Guerra <beatrice.guerra@zextras.com>
2023-02-27 20:25:48 +0100

    style: add plugin and libraries for styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
+    plugins: ['unused-imports'],
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parserOptions: {
         ecmaVersion: 2020,
@@ -17,5 +18,8 @@ module.exports = {
         'space-in-parens': 'error',
         'no-multiple-empty-lines': 'error',
         'prefer-const': 'error',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
     },
 };
